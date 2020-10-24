a94ba042f555c9e2997c46ad29f22c3b840cec23
zhussupali <zhussupali@users.noreply.github.com>
2020-10-24 18:54:53 +0500

    new view controller

---
+++ EmojiArtViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiArtViewController.swift
+//  EmojiArt
+//
+//  Created by Zhussupali Salamat on 10/24/20.
+//
+
+import UIKit
+
+class EmojiArtViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
