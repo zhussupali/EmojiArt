f655a0801235aa7dfa89ee4dfc6417523ac54fe5
zhussupali <zhussupali@users.noreply.github.com>
2020-10-25 16:30:57 +0500

    dropInteraction

---
+++ EmojiArt/EmojiArtView.swift
@@ -0,0 +1,20 @@
+//
+//  EmojiArtView.swift
+//  EmojiArt
+//
+//  Created by Zhussupali Salamat on 10/25/20.
+//
+
+import UIKit
+
+class EmojiArtView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- EmojiArtViewController.swift
+++ EmojiArtViewController.swift
@@ -7,14 +7,34 @@
 
 import UIKit
 
-class EmojiArtViewController: UIViewController {
+class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dropZone: UIView! {
+        didSet {
+            dropZone.addInteraction(UIDropInteraction(delegate: self))
+        }
+    }
+    @IBOutlet weak var emojiArtView: EmojiArtView!
+    
+    
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        return session.canLoadObjects(ofClass: NSURL.self) && session.canLoadObjects(ofClass: UIImage.self)
     }
     
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        return UIDropProposal(operation: .copy)
+    }
+    
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        session.loadObjects(ofClass: NSURL.self) {
+            urls in
+            
+        }
+        session.loadObjects(ofClass: UIImage.self) {
+            images in
+        }
+    }
 
     /*
     // MARK: - Navigation
