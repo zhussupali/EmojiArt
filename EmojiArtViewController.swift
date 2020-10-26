da5587210ddcaf176ca3ef8c8ddd60b5b87a66ef
zhussupali <zhussupali@users.noreply.github.com>
2020-10-26 14:39:33 +0500

    dragging photos

--- EmojiArtViewController.swift
+++ EmojiArtViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
-
-
+    
+    
     @IBOutlet weak var dropZone: UIView! {
         didSet {
             dropZone.addInteraction(UIDropInteraction(delegate: self))
@@ -26,24 +26,36 @@ class EmojiArtViewController: UIViewController, UIDropInteractionDelegate {
         return UIDropProposal(operation: .copy)
     }
     
+    var imageFetcher : ImageFetcher!
+    
     func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
-        session.loadObjects(ofClass: NSURL.self) {
-            urls in
-            
+        imageFetcher = ImageFetcher() { (url, image) in
+            DispatchQueue.main.async {
+                self.emojiArtView.backgroundImage = image
+            }
         }
-        session.loadObjects(ofClass: UIImage.self) {
-            images in
+        
+        session.loadObjects(ofClass: NSURL.self) { nsurls in
+            if let url = nsurls.first as? URL {
+                self.imageFetcher.fetch(url)
+            }
+        }
+        session.loadObjects(ofClass: UIImage.self) { images in
+            if let image = images.first as? UIImage {
+                self.imageFetcher.backup = image
+                
+            }
         }
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
